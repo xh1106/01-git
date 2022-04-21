ed7345205d9adf1f81b0f18aa49123d58f85762e
xiaohao <1927701569@qq.com>
2022-04-21 21:57:23 +0800

    first commit

---
+++ src/main/java/com/xh/Test.java
@@ -0,0 +1,14 @@
+package com.xh;
+
+/**
+ * @Description:
+ * @Author: xiaohao
+ * @Time: 2022/4/21 21:54
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("-----------");
+        System.out.println("-----------");
+
+    }
+}
